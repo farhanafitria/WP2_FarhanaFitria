3f80e76348788471b4c9c3db59fb733e74e93f34
farhanafitria <frhnafitria@gmail.com>
2021-09-20 14:05:17 +0700

    upload awal

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extend CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Farhana Fitria 
+              Saya tinggal di daerah pontianak
+              Saya sangat suka menonton film";
+    }
+} 
