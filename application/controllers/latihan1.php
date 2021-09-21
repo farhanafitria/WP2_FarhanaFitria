e1fa213530b5050def5b39ef7d51e48a50aae5c4
farhanafitria <frhnafitria@gmail.com>
2021-09-21 11:16:39 +0700

    upload project

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,24 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<b><h2>Selamat Datang.. selamat belajar Web Programming";
+    }
+    
+        public function penjumlahan($n1, $n2)
+        {
+            /*$this->load->model('model_latihan1');
+            $hasil = $this->model_latihan1->jumlah($n1, $n2);
+            echo "Hasil Penjumlahan Dari " . $n1 . " + " . $n2 . " = " . $hasil;
+            */
+            
+
+            $this->load->model('model_latihan1');
+            $data['nilai1'] = $n1;
+            $data['nilai2'] = $n2;
+            $data['hasil'] = $this->model_latihan1->jumlah($n1, $n2);
+
+            $this->load->view('view-latihan1', $data);
+     }
+}
